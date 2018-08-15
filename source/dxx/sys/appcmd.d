9cd0e2a0116a6901285d67d82650700d0a2c9198
Mark Fisher <logicfish@gmail.com>
2018-08-15 03:01:19 +0100

    apps

--- source/dxx/sys/appcmd.d
+++ source/dxx/sys/appcmd.d
@@ -1,22 +1,23 @@
 module dxx.sys.appcmd;
 
+private import std.path;
+
 //version(Win64) {
 version(Windows) {
-  version(WinPTY) {
+  //version(WinPTY) {
     enum winpty = "winpty";
-  } else {
-    enum winpty = "";
-  }
+  //} else {
+  //  enum winpty = "";
+  //}
   enum dubopt = " --arch=x86_64 ";
   enum dmdopt = " -m64 ";
   enum _dub = winpty ~ " dub";
-  enum _rdmd = "rdmd -Iscriptlike-0.10.2/scriptlike/src";
+  enum _rdmd = "rdmd -Iscriptlike-0.10.2/scriptlike/src" ~ pathSeparator ~ "source";
   enum _git = winpty ~ " git";
 } else {
     enum dubopt = "";
     enum dmdopt = "";
     enum _dub = "dub";
-    enum _rdmd = "rdmd -Iscriptlike-0.10.2/scriptlike/src";
+    enum _rdmd = "rdmd -Iscriptlike-0.10.2/scriptlike/src" ~ pathSeparator ~ "source";
     enum _git = "git";
 }
-
--- tools/fetch.d
+++ tools/fetch.d
@@ -1,3 +1,17 @@
-module dxx.sys.tools.fetch;
+#!rdmd -I"scriptlike-0.10.2/scriptlike/src;source"
 
+module tools.fetch;
 
+private import std.stdio;
+private import std.path;
+private import std.getopt;
+private import std.array;
+private import scriptlike;
+
+private import dxx.sys.appcmd;
+
+void main(string[] args)
+{
+    tryRun(_git ~ " pull");
+    tryRun(_rdmd ~ dmdopt ~ " tools/bootstrap.d");
+}
