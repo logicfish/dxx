575c661759ae843ed792f7f275e0c8da1688d2ce
Mark Fisher <logicfish@gmail.com>
2018-08-16 20:20:18 +0100

    examples

--- examples/basic/source/app.d
+++ examples/basic/source/app.d
@@ -3,6 +3,18 @@ module app;
 import std.stdio;
 import dxx.app;
 
+class Example {
+};
+
+class BasicAppContext : ApplicationDefaultContext {
+    //override void registerAppDependencies(DefaultInjector injector) {
+      //injector.register!(DObjectImpl,DObject);
+      //DCoreModelImpl.registerPackage(injector);
+      //injector.register!Example;
+    //}
+
+};
+
 void main()
 {
 	writeln("Edit source/app.d to start your project.");
